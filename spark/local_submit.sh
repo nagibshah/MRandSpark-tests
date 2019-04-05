fe0e2bc990929e17ea111023e04876553bdd0576
Nagib Shah <nagib.shah@gmail.com>
2019-04-05 19:10:55 +1100

    spark submit local + shell script

--- spark/local_submit.sh
+++ spark/local_submit.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "Invalid number of parameters!"
+    echo "Usage: ./local_submit.sh [enter path to input file] [enter path to output folder]"
+    echo "Ensure all paths have the appropriate suffix - e.g. file://<full path>"
+    exit 1
+fi
+
 spark-submit \
     --master local[2] \
-    AverageRatingPerGenre.py
-    
+    Top10FastestDislikes_SparkJob.py --input $1 --output $2
